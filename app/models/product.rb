103efbddf70e3313e6cc020371fbb0223fdf322f
Steve Klabnik <steve@steveklabnik.com>
2013-08-01 11:45:19 -0400

    iteration 2 is done

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,12 @@
 class Product < ActiveRecord::Base
+  validates_numericality_of :price
+  validates :stock, numericality: { 
+    only_integer: true,
+    greater_than_or_equal_to: 0
+  }
+
+  def price=(new_price)
+    new_price.delete!("$")
+    super
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130726150609) do
+ActiveRecord::Schema.define(version: 20130801133217) do
 
   create_table "products", force: true do |t|
     t.string   "title"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 20130726150609) do
     t.string   "image_url"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "stock",                               default: 0, null: false
   end
 
 end
